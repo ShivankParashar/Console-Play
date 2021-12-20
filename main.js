ac45a12483a24831d21c99637597bedcd174032b
ShivankParashar <83078938+ShivankParashar@users.noreply.github.com>
2021-12-20 21:31:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function upcon() {
+    console.log("My name is:- Shivank Parashar")
+    console.log("I am 12 years old")
+    console.log("I study in 6th Grade")
+    console.log("I am in section A")
+    console.log("I am in Cambridge Program")
+    console.log("I study in Neerja Modi School")
+    console.log("I live in Jaipur")
+}
